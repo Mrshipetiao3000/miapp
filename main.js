51b9d61d1ebdf5b9db31d56141fb3827bacb1b63
Mrshipetiao3000 <jesustar.ps4@gmail.com>
2025-07-14 15:07:41 -0400

    add sequencial id

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const formulario = document.querySelector(".form-create");
+let contador = 1;
+
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     const publicacion = document.querySelector("#publicacion").value;
     const contenedor = document.querySelector(".contenedor-publicaciones");
-    contenedor.innerHTML += `
-        <div class="publicacion">
-            <p>Id: 00</p>
+    contenedor.innerHTML += `<div class="publicacion">
+            <p> id ${contador}</p>
             <p>${publicacion}</p>
             <button>Editar</button>
             <button>Eliminar</button>
         </div>`
+
+        contador++;
 });
